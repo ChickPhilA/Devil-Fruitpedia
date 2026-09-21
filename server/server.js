3574820e331d721c341750759eda770718706576
Phil <natsudragoneel282003@gmail.com>
2026-09-20 19:53:58 -0700

    Implemented a server file with routing in the root as well as a wildcard/404 route.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 import express from 'express'
 const app = express()
 
-app.use('/public', express.static('./public'))
-app.use('/scripts', express.static('./public/scripts'))
+app.use('/public', express.static('../client/src/public'))
+// app.use('/scripts', express.static('./public/scripts'))
 
 app.get('/', (req, res) => {
     res.status(200).send("<h1> Success!! </h1>")
 })
 
-app.listen(3000, () => {
-    console.log("Server now running on Port 3000! Yippee!!!")
+// In Express v5 and up, the wildcard must have a name with it.
+app.get('*nothing', (req, res) => {
+    res.status(404).send("<h1>Error 404: Route not found</h1>")
+})
+
+const PORT =  process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log(`Server now running on Port ${PORT}! Yippee!!!`)
+    console.log(`Check it out here: http://localhost:${PORT}`)
 })
