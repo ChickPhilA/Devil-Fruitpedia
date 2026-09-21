43ac02b6c0c57b793de7ba57ee74439a7d98d4ca
Phil <natsudragoneel282003@gmail.com>
2026-09-21 13:05:01 -0700

    Added routing for Devil Fruit pages using the Express Router object.

---
+++ server/routes/devil_fruits.js
@@ -0,0 +1,24 @@
+// This file handles HTTP requests and controls routing across multipel files
+
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import devil_fruits from '../data/devil_fruits.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const router = express.Router()
+
+// When a router is pointed to this file, it'll return the json of all
+// the Devil Fruit data!
+// Type localhost:3000/devil_fruits and see what you get!
+router.get('/', (req, res) => {
+    res.status(200).json(devil_fruits)
+})
+
+router.get('/:fruitId', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, '../../client/index.html'))
+})
+
+export default router
