716e767d0433f7cfdda5e89448b75da7359b70c9
Phil <natsudragoneel282003@gmail.com>
2026-09-20 17:48:29 -0700

    Initial devil fruit data.

---
+++ server/data/devil_fruits.js
@@ -0,0 +1,70 @@
+// This is where our Devil Fruit data will be stored!
+
+const devil_fruits = [
+    {
+        id: 'gomu',
+        name: 'Gomu Gomu no Mi',
+        users: ['Monkey D. Luffy'],
+        description: 'Turns the eater\'s body into rubber, making them immune to blunt attacks and electricity. It was later revealed to be the Hito Hito no Mi, Model: Nika.',
+        type: 'Paramecia',
+        picture: '/public/assets/gomu.png'
+    },
+    {
+        id: 'mera',
+        name: 'Mera Mera no Mi',
+        users: ['Sabo', 'Portgas D. Ace'],
+        description: 'Lets the eater create, control and become fire. It was eaten by Ace, and after his death it was won by Sabo in the Dressrosa tournament.',
+        type: 'Logia',
+        picture: '/public/assets/mera.png'
+    },
+    {
+        id: 'hie',
+        name: 'Hie Hie no Mi',
+        users: ['Kuzan (Aokiji)'],
+        description: 'Lets the eater create, control and become ice. It can freeze large areas of the sea in an instant.',
+        type: 'Logia',
+        picture: '/public/assets/hie.png'
+    },
+    {
+        id: 'yami',
+        name: 'Yami Yami no Mi',
+        users: ['Marshall D. Teach (Blackbeard)'],
+        description: 'Lets the eater create and control darkness. It can pull in and nullify other Devil Fruit powers, at the cost of making the user take more damage.',
+        type: 'Logia',
+        picture: '/public/assets/yami.png'
+    },
+    {
+        id: 'gura',
+        name: 'Gura Gura no Mi',
+        users: ['Marshall D. Teach (Blackbeard)', 'Edward Newgate (Whitebeard)'],
+        description: 'Lets the eater create powerful tremors, and is said to be capable of destroying the world. Blackbeard took it after Whitebeard\'s death.',
+        type: 'Paramecia',
+        picture: '/public/assets/gura.png'
+    },
+    {
+        id: 'hana',
+        name: 'Hana Hana no Mi',
+        users: ['Nico Robin'],
+        description: 'Lets the eater sprout copies of their body parts from any surface, letting them attack from anywhere.',
+        type: 'Paramecia',
+        picture: '/public/assets/hana.png'
+    },
+    {
+        id: 'hito',
+        name: 'Hito Hito no Mi',
+        users: ['Tony Tony Chopper'],
+        description: 'Gives the eater human intelligence and the ability to become a human or a human-animal hybrid. Chopper ate it as a reindeer.',
+        type: 'Zoan',
+        picture: '/public/assets/hito.png'
+    },
+    {
+        id: 'ope',
+        name: 'Ope Ope no Mi',
+        users: ['Trafalgar D. Water Law'],
+        description: 'Lets the eater create a spherical operating room where they can cut, rearrange and swap anything inside. It is known as the ultimate Devil Fruit.',
+        type: 'Paramecia',
+        picture: '/public/assets/ope.png'
+    }
+]
+
+export default devil_fruits
