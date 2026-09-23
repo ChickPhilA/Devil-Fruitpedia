2f535ccdf1902045498e18c4687096372339465b
Phil <natsudragoneel282003@gmail.com>
2026-09-22 17:26:25 -0700

    Updated sendFile for 404 page; might adjust later to avoid Vite crashing

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import dfRouter from './routes/devil_fruits.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 
@@ -9,12 +14,12 @@ app.use('/public', express.static('../client/src/public'))
 app.use('/devil_fruits', dfRouter) // devil fruit router
 
 app.get('/', (req, res) => {
-    res.status(200).send("<h1> Success!! </h1>")
+    res.status(200).send('<h1> SUCCESS!!! </h1>')
 })
 
 // In Express v5 and up, the wildcard must have a name with it.
 app.get('*nothing', (req, res) => {
-    res.status(404).send("<h1>Error 404: Route not found</h1>")
+    res.status(404).sendFile(path.resolve(__dirname, '../client/src/public/404.html'))
 })
 
 const PORT =  process.env.PORT || 3001
